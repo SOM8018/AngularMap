6218a80ffd6726f011b5e7e51aa527df3197a366
Soumyaranjan Mohanty <soumya8018@gmail.com>
2018-11-23 10:02:41 +0530

    added and updated the .forRoot api key

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mapAngular';
+  latitude = 17.468915 ;
+  longitude = 78.367789 ;
+
+  onChooseLocation(event)
+  {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey : 'AIzaSyBL5wJ2hc_uqClG9qO1sRwb-IN0wY0Au3M',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
